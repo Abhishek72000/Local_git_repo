21e23db8215cd808306e9d724bd904cb9ff08ccc
Bhushan Junagade <chaudhariabhishek20@gmail.com>
2025-03-06 20:23:40 +0530

    first commit

---
+++ Sample.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUser([FromQuery] int id)
+        {
+            return Ok(new { id, name = "example" });
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] string name)
+        {
+            return Ok(new { id = 1, name });
+        }
+    }
+}
