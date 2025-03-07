0812a5f8104a4b908e4f7a214c6776ea91c0f61d
Bhushan Junagade <chaudhariabhishek20@gmail.com>
2025-03-07 23:23:58 +0530

    tenth commit

---
+++ ProgramController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class ProgramController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult EvenAndOdd([FromQuery] int id)
+        {
+
+           if (id % 2 == 0)
+        {
+            Console.WriteLine($"{number} is Even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is Odd.");
+        }
+        }
+
+    }
+}
