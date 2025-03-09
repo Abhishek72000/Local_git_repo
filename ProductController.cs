d119c3329b07994560a7e74b554d41a2638c2a8b
Bhushan Junagade <chaudhariabhishek20@gmail.com>
2025-03-09 13:28:24 +0530

    thirtyone commit

--- ProductController.cs
+++ ProductController.cs
@@ -22,7 +22,31 @@ namespace MyApi.Controllers
         }
         }
 
-       
+        [HttpGet("{num}")]
+        public IActionResult FibonacciSeries([FromQuery]int num){
+             int n;
+            Console.Write("Enter the number of terms: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series up to " + n + " terms:");
+            
+            int firstTerm = 0, secondTerm = 1, nextTerm;
+
+            if (n >= 1)
+                Console.Write(firstTerm + " ");
+            if (n >= 2)
+                Console.Write(secondTerm + " ");
+
+            for (int i = 3; i <= n; i++)
+            {
+                nextTerm = firstTerm + secondTerm;
+                Console.Write(nextTerm + " ");
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+
+            Console.WriteLine(); // for newline
+        }
     }
     
 }
