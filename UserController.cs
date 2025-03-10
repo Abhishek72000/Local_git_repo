29d49b5bfec2a7d1dfdd58d5be6ed07d2a9a7d7b
Bhushan Junagade <chaudhariabhishek20@gmail.com>
2025-03-10 08:57:07 +0530

    thirty38 commit

--- UserController.cs
+++ UserController.cs
@@ -18,4 +18,30 @@ namespace MyApi.Controllers
         }
 
     }
+    [HttpGet("getprogram/{id}")]
+        public IActionResult GetProgram(int id)
+        {
+            var program = _programs.FirstOrDefault(p => p.Id == id);
+
+            if (program == null)
+            {
+                return NotFound(new { Message = "Program not found." });
+            }
+
+            return Ok(program);
+        }
+
+
+        [HttpGet("getauthor/{id}")]
+        public IActionResult GetAuthor(int id)
+        {
+            var author = _authors.FirstOrDefault(a => a.Id == id);
+
+            if (author == null)
+            {
+                return NotFound(new { Message = "Author not found." });
+            }
+
+            return Ok(author);
+        }
 }
